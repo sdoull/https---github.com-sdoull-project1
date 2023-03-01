238b8349d0df0bf7f7dff769c627e96b9ceb3f09
Stephanie Doull <stephaniedoull@hotmail.com>
2023-03-01 09:17:08 -0400

    added comments

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
-
+  /* for the menu function*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "450px";
   }
   
-  /* Set the width of the side navigation to 0 */
+
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
