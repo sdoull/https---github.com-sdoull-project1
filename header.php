f619cb7f7fac9f12bfaf24128950068594a353ca
Stephanie Doull <stephaniedoull@hotmail.com>
2023-05-24 20:58:57 -0300

    fix

--- header.php
+++ header.php
@@ -20,16 +20,10 @@
 
 <?php require 'menu-bar.php';?>
 <body>
-    <!-- Attempt MySQL server connection. Assuming you are running MySQL server with default setting (user 'root' with no password)
-DB is skillsprojet3
-Table trips -->
     <?php
-    $conn = mysqli_connect("localhost", "root", "", "https---github.com-sdoull-project1", );
+    $conn = mysqli_connect("localhost", "root", "", "skillsproject3", );
     // check connection
     if($conn === false){
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
     ?>
-
-        
-    </body>
